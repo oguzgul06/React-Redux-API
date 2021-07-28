cccaccd7bf05ab13f5bb000dd1bd7737c4db3d84
oguz gül <ogzgl95@outlook.com>
2021-07-28 16:46:13 +0300

    reducer component

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import { Route } from "react-router-dom";
 
 import Products from "./components/Products";
 import Cart from "./components/Cart";
+import { connect } from "react-redux";
 
-import { data } from "./data";
-
-export default function App() {
+const App = (props) => {
   return (
     <div className="App">
       <h1>
-        Alışveriş Sepeti Yapımı
+        Shopping Cart
         <img
           src="https://avatars3.githubusercontent.com/u/60869810?v=4"
           alt="React Dersleri"
         />{" "}
-        React Dersleri
+        
       </h1>
       <Route exact path="/" component={Products} />
       <Route path="/cart" component={Cart} />
     </div>
   );
-}
+};
+
+const mapStateToProps = (state) => {
+  return { bookList: state.bookList };
+};
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { createStore } from "redux";
+import { reducer } from "./reducers/reducer";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
