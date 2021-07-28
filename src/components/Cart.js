6f3c7d0c5249e4ce25b232bac76ff055816c9a00
oguz gül <ogzgl95@outlook.com>
2021-07-28 17:35:51 +0300

    Entegrated redux of the cart components

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,33 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Cart = () => {
+const Cart = (props) => {
+  const totalPrice = props.cart.reduce(
+    (total, item) => (total += item.price),
+    0
+  );
+
   return (
     <div>
       <h2>
-        <Link to="/">Kitap Listesi</Link> <span>Sepetim</span>
+        <Link to="/">Book List</Link> <span>My Cart</span>
       </h2>
 
-      <h3>Toplam Sepet Tutarı: &#8378;19.99</h3>
+      <h3>Total Cart Price: &#8378;{totalPrice.toFixed(2)}</h3>
 
-      <div className="book">
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
-          alt="Simyacı"
-        />
-        <div>
-          <h4>Simyaci</h4>
-          <p>Yazar: Paulo Coelho</p>
-          <p>Fiyat: &#8378;19.99</p>
-          <p>Toplam: &#8378;19.99</p>
-          <p>Sepetinizde bu kitaptan toplam 1 adet var.</p>
-          <button>-</button>
-          <button>Sepetten Çıkar</button>
-          <button>+</button>
+      {props.cart.map((book) => (
+        <div className="book" key={book.id}>
+          <img src={book.image} alt={book.name} />
+          <div>
+            <h4>{book.name}</h4>
+            <p>Author: {book.author}</p>
+            <p>Price: &#8378;{book.price}</p>
+
+            <button>-</button>
+            <button>Out of The Bag</button>
+            <button>+</button>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
 
-export default Cart;
+const mapStateToProps = (state) => {
+  return { cart: state.cart };
+};
+
+export default connect(mapStateToProps)(Cart);
